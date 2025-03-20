050f27bcfafa455040323af5ae6f2525bee036db
Mike <115405590+michaelbarkasi@users.noreply.github.com>
2025-03-20 16:08:48 -0500

    Added function for searching LROcp settings

--- scratch.R
+++ scratch.R
@@ -1,7 +1,11 @@
 
+# Set for debugging (if needed): Sys.setenv(CXXFLAGS="-fsanitize=address -g -O1")
+# ... in external terminal: cd to working directory, run "scratch.R"
 
 rm(list = ls())
+Sys.setenv(CXXFLAGS="-fsanitize=address -g -O1")
 projects_folder <- "/Users/michaelbarkasi/Library/CloudStorage/OneDrive-WashingtonUniversityinSt.Louis/projects_Oviedo_lab/"
+sink("output.txt", split = TRUE, append = FALSE, type = "output")
 
 # Set random seed for reproducibility
 # ... R only. C++ seed set in its code
@@ -61,7 +65,8 @@ model.settings = list(
   ctol = 1e-6,                                          # convergence tolerance
   max_penalty_at_distance_factor = 0.01,                # maximum penalty at distance from structural parameter values
   LROcutoff = 2.0,                                      # cutoff for LROcp
-  LROwindow_factor = 2.0,                               # window factor for LROcp
+  LROwindow_factor = 2.0,                               # window factor for LROcp, larger means larger rolling window
+  LROfilter_ws_divisor = 2.0,                           # divisor for filter window size in likelihood ratio outlier detection (bigger is smaller window)
   tslope_initial = 1.0,                                 # initial value for tslope
   wf_initial = 0.15,                                    # initial value for wfactor
   max_evals = 1000,                                     # maximum number of evaluations for optimization
@@ -80,18 +85,83 @@ model.settings = list(
 # - Adjusting the LROcutoff is another way of controlling how the model finds rate transitions. Higher values 
 #    mean fewer detected transitions. 
 
-merfish.laminar.model <- wisp(
-  count.data.raw = countdata,
-  variables = data.variables,
-  use.median = FALSE,
-  bootstraps.num = 1e2,
-  converged.resamples.only = FALSE,
-  max.fork = bs_chunksize,
-  batch.size = bs_chunksize,
-  dim.bounds = layer.boundary.bins,
-  verbose = TRUE,
-  print.child.summaries = TRUE, 
-  model.settings = model.settings
-)
+# merfish.laminar.model <- wisp(
+#   count.data.raw = countdata,
+#   variables = data.variables,
+#   use.median = FALSE,
+#   bootstraps.num = 0,
+#   converged.resamples.only = FALSE,
+#   max.fork = bs_chunksize,
+#   batch.size = bs_chunksize,
+#   dim.bounds = layer.boundary.bins,
+#   verbose = TRUE,
+#   print.child.summaries = TRUE, 
+#   model.settings = model.settings
+# )
+
+
+scan_LRO <- function() {
+  
+  ct <- c(1.5, 2.0, 2.5, 3.0)
+  wf <- c(2.0, 3.0, 4.0)
+  wd <- c(2.0, 3.0, 4.0)
+  grd <- expand.grid(ct, wf, wd)
+  
+  time <- Sys.time()
+  
+  cps <- list()
+  res <- list()
+  bs <- list()
+  for (r in 1:nrow(grd)) {
+    
+    model.settings = list(
+      struc_values = c(                                     # values of structural parameters to test
+        5.0,   # beta_shape_point
+        5.0,   # beta_shape_rate
+        1.0    # sd_tslope_effect
+      ),  
+      buffer_factor = 0.05,                                 # buffer factor for penalizing distance from structural parameter values
+      ctol = 1e-6,                                          # convergence tolerance
+      max_penalty_at_distance_factor = 0.01,                # maximum penalty at distance from structural parameter values
+      LROcutoff = grd[r,1],                                      # cutoff for LROcp
+      LROwindow_factor = grd[r,2],                               # window factor for LROcp, larger means larger rolling window
+      LROfilter_ws_divisor = grd[r,3],                           # divisor for filter window size in likelihood ratio outlier detection (bigger is smaller window)
+      tslope_initial = 1.0,                                 # initial value for tslope
+      wf_initial = 0.15,                                    # initial value for wfactor
+      max_evals = 1000,                                     # maximum number of evaluations for optimization
+      initial_fits = 10,                                    # number of initial fits to perform in search of best initial conditions and best fit
+      rng_seed = 42                                         # random seed for optimization (controls bootstrap resamples only)
+    )
+    
+    m <- wisp(
+      count.data.raw = countdata,
+      variables = data.variables,
+      use.median = FALSE,
+      bootstraps.num = 0,
+      converged.resamples.only = FALSE,
+      max.fork = bs_chunksize,
+      batch.size = bs_chunksize,
+      dim.bounds = layer.boundary.bins,
+      verbose = FALSE,
+      print.child.summaries = FALSE, 
+      model.settings = model.settings
+    )
+    
+    cps[[r]] <- m$change.points 
+    res[[r]] <- m$stats$residuals.log
+    bs[[r]] <- m$bs.diagnostics[nrow(m$bs.diagnostics),]
+    
+    d <- Sys.time() - time
+    cat("Finished", r, "of", nrow(grd), "in", d, units(d), "\n")
+    time <- Sys.time()
+    
+  }
+  
+  return(list(cps = cps, res = res, bs = bs, grd = grd))
+  
+}
+
+LROscan_res <- scan_LRO()
+
 
 
