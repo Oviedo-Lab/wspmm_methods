57ef4eb847a88da56d05ab6050267ddde597c7ef
Mike <115405590+michaelbarkasi@users.noreply.github.com>
2025-04-26 22:22:59 -0500

    Added new settings for stats

---
+++ Untitled.cpp
@@ -0,0 +1,36 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// This is a simple example of exporting a C++ function to R. You can
+// source this function into an R session using the Rcpp::sourceCpp 
+// function (or via the Source button on the editor toolbar). Learn
+// more about Rcpp at:
+//
+//   http://www.rcpp.org/
+//   http://adv-r.had.co.nz/Rcpp.html
+//   http://gallery.rcpp.org/
+//
+
+// [[Rcpp::export]]
+bool pattern_match(
+    std::string pattern,
+    std::string test
+) {
+  bool match = false;
+  if (test.find(pattern) != std::string::npos) {
+    match = true; 
+  }
+  return match;
+}
+
+
+// You can include R code blocks in C++ files processed with sourceCpp
+// (useful for testing and development). The R code will be automatically 
+// run after the compilation.
+//
+
+/*** R
+pattern_match(
+  "z", "abc"
+)
+*/
